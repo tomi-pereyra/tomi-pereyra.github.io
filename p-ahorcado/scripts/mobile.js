a735d474a1e50217cb99a3e160fa1b730eef596a
Tomás Pereyra <eltokier@gmail.com>
2022-02-04 12:46:35 -0300

    archivo faltante

---
+++ p-ahorcado/scripts/mobile.js
@@ -0,0 +1,163 @@
+var a = document.querySelector(".btn-a");
+var b = document.querySelector(".btn-b");
+var c = document.querySelector(".btn-c");
+var d = document.querySelector(".btn-d");
+var e = document.querySelector(".btn-e");
+var f = document.querySelector(".btn-f");
+var g = document.querySelector(".btn-g");
+var h = document.querySelector(".btn-h");
+var i = document.querySelector(".btn-i");
+var j = document.querySelector(".btn-j");
+var k = document.querySelector(".btn-k");
+var l = document.querySelector(".btn-l");
+var m = document.querySelector(".btn-m");
+var n = document.querySelector(".btn-n");
+var ñ = document.querySelector(".btn-ñ");
+var o = document.querySelector(".btn-o");
+var p = document.querySelector(".btn-p");
+var q = document.querySelector(".btn-q");
+var r = document.querySelector(".btn-r");
+var s = document.querySelector(".btn-s");
+var t = document.querySelector(".btn-t");
+var u = document.querySelector(".btn-u");
+var v = document.querySelector(".btn-v");
+var w = document.querySelector(".btn-w");
+var x = document.querySelector(".btn-x");
+var y = document.querySelector(".btn-y");
+var z = document.querySelector(".btn-z");
+
+a.addEventListener("click", enviarA);
+b.addEventListener("click", enviarB);
+c.addEventListener("click", enviarC);
+d.addEventListener("click", enviarD);
+e.addEventListener("click", enviarE);
+f.addEventListener("click", enviarF);
+g.addEventListener("click", enviarG);
+h.addEventListener("click", enviarH);
+i.addEventListener("click", enviarI);
+j.addEventListener("click", enviarJ);
+k.addEventListener("click", enviarK);
+l.addEventListener("click", enviarL);
+m.addEventListener("click", enviarM);
+n.addEventListener("click", enviarN);
+ñ.addEventListener("click", enviarÑ);
+o.addEventListener("click", enviarO);
+p.addEventListener("click", enviarP);
+q.addEventListener("click", enviarQ);
+r.addEventListener("click", enviarR);
+s.addEventListener("click", enviarS);
+t.addEventListener("click", enviarT);
+u.addEventListener("click", enviarU);
+v.addEventListener("click", enviarV);
+w.addEventListener("click", enviarW);
+x.addEventListener("click", enviarX);
+y.addEventListener("click", enviarY);
+z.addEventListener("click", enviarZ);
+
+function enviarA() {
+    vLetra("A");
+}
+
+function enviarB() {
+    vLetra("B");
+}
+
+function enviarC() {
+    vLetra("C");
+}
+
+function enviarD() {
+    vLetra("D");
+}
+
+function enviarE() {
+    vLetra("E");
+}
+
+function enviarF() {
+    vLetra("F");
+}
+
+function enviarG() {
+    vLetra("G");
+}
+
+function enviarH() {
+    vLetra("H");
+}
+
+function enviarI() {
+    vLetra("I");
+}
+
+function enviarJ() {
+    vLetra("J");
+}
+
+function enviarK() {
+    vLetra("K");
+}
+
+function enviarL() {
+    vLetra("L");
+}
+
+function enviarM() {
+    vLetra("M");
+}
+
+function enviarN() {
+    vLetra("N");
+}
+
+function enviarÑ() {
+    vLetra("Ñ");
+}
+
+function enviarO() {
+    vLetra("O");
+}
+
+function enviarP() {
+    vLetra("P");
+}
+
+function enviarQ() {
+    vLetra("Q");
+}
+
+function enviarR() {
+    vLetra("R");
+}
+
+function enviarS() {
+    vLetra("S");
+}
+
+function enviarT() {
+    vLetra("T");
+}
+
+function enviarU() {
+    vLetra("U");
+}
+
+function enviarV() {
+    vLetra("V");
+}
+
+function enviarW() {
+    vLetra("W");
+}
+
+function enviarX() {
+    vLetra("X");
+}
+
+function enviarY() {
+    vLetra("Y");
+}
+
+function enviarZ() {
+    vLetra("Z");
+}
